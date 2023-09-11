00f303351da19bf187094b86e577a984284c4496
tekenscodr <ensleybdarku@gmail.com>
2023-09-11 12:19:54 +0000

    done

--- app/backjob/percentage.ts
+++ app/backjob/percentage.ts
@@ -1,17 +1,14 @@
 
 export async function getPercentage() {
-    try {
-    setInterval(async() =>{
-      const res = await fetch('http://localhost:2024/auth/percentage', { next: { revalidate: 36 } })
-        if (!res.ok){
-          throw new Error('No votes loaded')
-        }
-        return res.json()
-        }, 2000) //2seconds
+  try{
+    const res = await fetch('http://localhost:2024/auth/percentage')
+    if (!res.ok){
+     throw new Error('No votes loaded')
+    }
+    return res.json()
     } catch (error) {
       throw new Error(`Failed: ${error}`)
     }
-      
 }
 
 
